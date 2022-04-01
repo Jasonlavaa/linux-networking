b9e201ce3570f85f5b3d8e67ae3a7b633856cd15
Jasonlavaa <99443487+Jasonlavaa@users.noreply.github.com>
2022-04-01 14:00:14 -0500

    Update makeRouter.sh

--- makeRouter.sh
+++ makeRouter.sh
@@ -1,3 +1,5 @@
 nmcli d 
 echo “1” >  /proc/sys/net/ipv4/ip_forward
- 
+iptables -A FORWARD -i eth1 -j ACCEPT 
+iptables -A FORWARD -o eth1 -j ACCEPT 
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
