b11cd67c4b427f2c82606fb69a33cd7a06cc0c96
Jasonlavaa <99443487+Jasonlavaa@users.noreply.github.com>
2022-04-01 14:20:36 -0500

    Update makeRouter.sh

--- makeRouter.sh
+++ makeRouter.sh
@@ -1,7 +1,7 @@
 ifconfig  <arg2> 20.30.40.1
 ifconfig  <arg2> netmask 255.255.255.0 
 ifconfig  <arg2> broadcast 20.30.40.255
-echo “1” >  /proc/sys/net/ipv4/ip_forward
+echo “1”   /proc/sys/net/ipv4/ip_forward
 iptables -A FORWARD -i eth1 -j ACCEPT 
 iptables -A FORWARD -o eth1 -j ACCEPT 
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
