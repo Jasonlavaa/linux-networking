a11a89c4761c81ed5659054eca8ea212d6293074
Jasonlavaa <99443487+Jasonlavaa@users.noreply.github.com>
2022-04-01 14:02:56 -0500

    Update makeRouter.sh

--- makeRouter.sh
+++ makeRouter.sh
@@ -1,4 +1,6 @@
-nmcli d 
+ifconfig  <arg2> 20.30.40.1
+ifconfig  <arg2> netmask 255.255.255.0 
+ifconfig  <arg2> broadcast 20.30.40.255
 echo “1” >  /proc/sys/net/ipv4/ip_forward
 iptables -A FORWARD -i eth1 -j ACCEPT 
 iptables -A FORWARD -o eth1 -j ACCEPT 
